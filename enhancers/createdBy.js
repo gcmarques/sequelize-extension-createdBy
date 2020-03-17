a0008f38250d93198dd9880ad7097f02cdf7952d
CareLuLu Gabriel <gabriel@carelulu.com>
2020-03-16 18:24:00 -0700

    fix(enhancer): update createdBy only if not set

--- enhancers/createdBy.js
+++ enhancers/createdBy.js
@@ -7,10 +7,12 @@ function enhanceCreatedBy() {
       if (utils.isModel(model) && _.has(utils.getRawAttributes(model), 'createdBy')) {
         const name = utils.getName(model);
         hooks[name].beforeCreate.push((instance, options) => {
-          instance.createdBy = options.user.id;
+          instance.createdBy = instance.createdBy || options.user.id;
         });
         hooks[name].beforeBulkCreate.push((instances, options) => {
-          _.each(instances, (instance) => { instance.createdBy = options.user.id; });
+          _.each(instances, (instance) => {
+            instance.createdBy = instance.createdBy || options.user.id;
+          });
         });
       }
     });
